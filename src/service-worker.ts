9439dc57167e67fde2e4353909045ac5e34007d8
Marcel Lee <chungchung1315@gmail.com>
2024-07-23 20:20:34 +0900

    fix: Load service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -78,28 +78,3 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
-
-self.addEventListener("install", function (e) {
-  console.log("fcm sw install..");
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", function (e) {
-  console.log("fcm sw activate..");
-});
-
-self.addEventListener("push", function (e) {
-  console.log("push: ", e.data?.json());
-
-  const resultData = e.data?.json().notification;
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.body,
-    icon: resultData.image,
-    tag: resultData.tag,
-    ...resultData,
-  };
-  console.log("push: ", { resultData, notificationTitle, notificationOptions });
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
