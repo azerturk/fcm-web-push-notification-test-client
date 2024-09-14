3879d690804bddc8dbb5bbb0167373e2471d62c0
magicportibp <magicport@ibp.az>
2024-09-14 09:14:54 +0400

    dsssadssslkjssasadksdadkss

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { fetchFirebaseToken } from "./messaging-get-token";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { onBackgroundMessage } from "firebase/messaging/sw";
 
