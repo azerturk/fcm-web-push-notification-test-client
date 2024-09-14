ee1f59fcd629c5deb038c4e61bb1112dff525dcf
magicportibp <magicport@ibp.az>
2024-09-14 10:37:27 +0400

    dsssadssslkjssasadksdadksssfldsfls

--- src/firebase-init.ts
+++ src/firebase-init.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
